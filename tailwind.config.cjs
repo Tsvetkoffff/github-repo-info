b73fc71e225c0cb844079f21e18aba1ddcadf47f
Slava Tsvetkoffff <v.tsvetkoffff@gmail.com>
2022-10-10 19:15:38 +0300

    feat: add primary color to config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2B4865'
+      }
+    },
   },
   plugins: [],
 }
