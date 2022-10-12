4c2b2919012e34a87e04c8adf15a137fcfdeb95d
Slava Tsvetkoffff <v.tsvetkoffff@gmail.com>
2022-10-12 20:14:31 +0300

    refactor: refactoring types of component and props

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
   children: React.ReactNode
 }
 
-const Container = ({children}: ContainerProps) => (
+const Container: React.FC<ContainerProps> = ({children}) => (
   <div className=' container mx-auto px-3 xl:px-4 2xl:px-5 '>{children}</div>
 )
 
