73de0865e6ca77c21fb09b90f87d74b5691ed670
Slava Tsvetkoffff <v.tsvetkoffff@gmail.com>
2022-10-13 12:36:42 +0300

    feat: get user repos by click on the login and render user repos url`s on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,8 @@
 import {useState, useEffect} from 'react'
-import {useSearchUsersQuery} from '../store/github/github.api'
+import {
+  useLazyGetUserReposQuery,
+  useSearchUsersQuery,
+} from '../store/github/github.api'
 import {useSuspend} from './../hooks/suspend'
 import Error from '../components/Error'
 
@@ -7,44 +10,74 @@ const HomePage = () => {
   const [search, setSearch] = useState('')
   const suspended = useSuspend(search)
   const [dropdown, setDropdown] = useState(false)
-  const {isLoading, isError, data} = useSearchUsersQuery(suspended, {
+
+  const {
+    isLoading: areUsersLoading,
+    isError: areUsersError,
+    data: users,
+  } = useSearchUsersQuery(suspended, {
     skip: suspended.length < 3,
   })
 
+  const [
+    fetchUserRepos,
+    {isLoading: areReposLoading, isError: areReposError, data: repos},
+  ] = useLazyGetUserReposQuery()
+
   useEffect(() => {
-    setDropdown(suspended.length >= 3 && data?.length! > 0)
-  }, [suspended, data])
+    setDropdown(suspended.length >= 3 && users?.length! > 0)
+  }, [suspended, users])
+
+  const userLoginClickHandler = (login: string) => {
+    return () => {
+      fetchUserRepos(login)
+      setDropdown(false)
+    }
+  }
 
   return (
-    <div className=' flex flex-col items-center mx-auto pt-10 '>
-      {isError && <Error />}
-      <div className=' relative w-full sm:w-[615px] flex flex-col justify-center '>
-        <input
-          type='text'
-          className=' h-[42px] w-full p-2 border border-slate-500'
-          placeholder='Search GitHub users here...'
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
-        {isLoading && (
-          <p className=' absolute top-[44px] left-0 right-0 text-center p-6'>
-            Loading...
-          </p>
-        )}
-        {dropdown && (
-          <ul className=' absolute top-[44px] left-0 right-0 max-h-[200px] shadow-md rounded-b-xl list-none overflow-y-scroll '>
-            {data?.map(item => (
-              <li
-                key={item.id}
-                className='hover:bg-slate-400 p-2 cursor-pointer'
-              >
-                {item.login}
-              </li>
-            ))}
-          </ul>
-        )}
+    <>
+      <div className=' flex flex-col items-center mx-auto pt-10 '>
+        {areUsersError && <Error />}
+        <div className=' relative w-full sm:w-[615px] flex flex-col justify-center '>
+          <input
+            type='text'
+            className=' h-[42px] w-full p-2 border border-slate-500'
+            placeholder='Search GitHub users here...'
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+          {areUsersLoading && (
+            <p className=' absolute top-[44px] left-0 right-0 text-center p-6'>
+              Loading...
+            </p>
+          )}
+          {dropdown && (
+            <ul className=' absolute top-[44px] left-0 right-0 max-h-[200px] shadow-md rounded-b-xl list-none overflow-y-scroll bg-white'>
+              {users?.map(user => (
+                <li
+                  key={user.id}
+                  onClick={userLoginClickHandler(user.login)}
+                  className='hover:bg-slate-400 p-2 cursor-pointer'
+                >
+                  {user.login}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-    </div>
+      <div>
+        {areReposError && <Error />}
+        {areReposLoading && <p>Loading...</p>}
+        {repos?.map(repo => (
+          <div key={repo.id}>
+            <p>{repo.url}</p>
+            <button className='border p-2'>Add to favorites</button>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,6 +1,6 @@
 // Need to use the React-specific entry point to allow generating React hooks
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import { IUser, ServerResponse } from '../../models/models'
+import {IRepo, IUser, ServerResponse} from '../../models/models'
 
 // Define a service using a base URL and expected endpoints
 export const githubApi = createApi({
@@ -8,18 +8,23 @@ export const githubApi = createApi({
   baseQuery: fetchBaseQuery({baseUrl: 'https://api.github.com'}),
   endpoints: builder => ({
     searchUsers: builder.query<IUser[], string>({
-      query: (search:string) => ({
+      query: (search: string) => ({
         url: `search/users`,
         params: {
           q: search,
-          per_page: 10
-        }
+          per_page: 10,
+        },
       }),
-      transformResponse: (response:ServerResponse<IUser>) => response.items
+      transformResponse: (response: ServerResponse<IUser>) => response.items,
+    }),
+    getUserRepos: builder.query<IRepo[], string>({
+      query: (user: string) => ({
+        url: `users/${user}/repos`,
+      }),
     }),
   }),
 })
 
 // Export hooks for usage in function components, which are
 // auto-generated based on the defined endpoints
-export const {useSearchUsersQuery} = githubApi
+export const {useSearchUsersQuery, useLazyGetUserReposQuery} = githubApi
